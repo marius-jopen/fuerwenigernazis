d4703fbe430a646e73cbb05f921e9fc41ca35c82
Marius Jopen <mail@mariusjopen.com>
2025-01-08 11:30:41 +0100

    logos

--- src/lib/slices/Logos/index.svelte
+++ src/lib/slices/Logos/index.svelte
@@ -12,7 +12,7 @@
 	<div class="grid grid-cols-2 lg:grid-cols-4 gap-8 px-8 pt-16">
 		{#each slice.primary.items as item}
 			<div class="pt-8 lg:pt-0">
-				<PrismicImage field={item.image} />
+				<PrismicImage class="w-full aspect-[16/9] object-contain grayscale" field={item.image} />
 			</div>
 		{/each}
 	</div>
